b78712321d0fc85c772be52434fb623343996431
zartre <paulpreuss@gmail.com>
2020-08-14 13:59:42 +0200

    "Color" Branch - einfacher, scrollender RecyclerView (keine actions) mit zwei Views im Viewholder, Text und Grafik

--- app/src/main/java/com/paul/beeterecyclerview/BeeteListAdapter.java
+++ app/src/main/java/com/paul/beeterecyclerview/BeeteListAdapter.java
@@ -1,11 +1,14 @@
 package com.paul.beeterecyclerview;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,19 +17,22 @@ import java.util.LinkedList;
 public class BeeteListAdapter extends RecyclerView.Adapter<BeeteListAdapter.BeeteViewHolder> {
 
     private final LinkedList<String> beeteArray;
+    private final int[] beeteImgArray;
     private LayoutInflater inflater; //handler/initialisiertes objekt für den Inflater der in onCreate verwendet wird
 
-    public BeeteListAdapter(Context context, LinkedList<String> beeteWorte) { //Konstruktor setzt 2 Klassen-Variablen
+    public BeeteListAdapter(Context context, LinkedList<String> beeteWorte, int[] beeteImgs) { //Konstruktor setzt 3 Klassen-Variablen
         this.beeteArray = beeteWorte;
+        this.beeteImgArray = beeteImgs;
         inflater = LayoutInflater.from(context); //LayoutInflater erhält einen context (für dimensionen aus activity?)
     }
 
     @NonNull
     @Override
-    //onCreate-Methode im Adapter (onCreateViewHolder??) erstellt den ViewHolder
+    //onCreate-Methode im Adapter erstellt den ViewHolder
     public BeeteListAdapter.BeeteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { //woher die viewgroup und ints kommen -> recycler interna
         View inflatedView = inflater.inflate(R.layout.beetliste_element, parent, false); //ein inflater macht
-        //mit einem XML ein View-Objekt
+        //mit einem XML ein View-Objekt. der inflated View enthält alle elemente aus beetliste.xml
+        //constraint layout funktioniert auch!
         return new BeeteViewHolder(inflatedView); //ab hier existiert unser viewholder
     }
 
@@ -36,6 +42,7 @@ public class BeeteListAdapter extends RecyclerView.Adapter<BeeteListAdapter.Beet
     {
         String currentPos = beeteArray.get(position); //interna..
         holder.beeteElementView.setText(currentPos);
+        holder.beeteImageView.setImageResource(beeteImgArray[position]);
     }
 
     @Override
@@ -46,10 +53,12 @@ public class BeeteListAdapter extends RecyclerView.Adapter<BeeteListAdapter.Beet
     public class BeeteViewHolder extends RecyclerView.ViewHolder {
 
         public final TextView beeteElementView; //Handler für 'View' des inneren Textview-Element
+        public final ImageView beeteImageView; //inneres Img-Element
 
         public BeeteViewHolder(View elementView) { //Konstruktor
             super(elementView); //initialisert einen ViewHolder (Konstruktor der Superklasse) mit BeetwordView
-            beeteElementView = elementView.findViewById(R.id.beetname); //speichert diesen View in der View-Variable des Viewholder
+            this.beeteElementView = elementView.findViewById(R.id.beetname); //speichert diesen View in der View-Variable des Viewholder
+            this.beeteImageView = elementView.findViewById(R.id.imageView2);//ImgView ist element von elementView
         }
 
 
--- app/src/main/java/com/paul/beeterecyclerview/MainActivity.java
+++ app/src/main/java/com/paul/beeterecyclerview/MainActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import java.util.LinkedList;
 
@@ -18,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
     private BeeteListAdapter myBeeteListAdapter;
 
     private final LinkedList<String> beetListe = new LinkedList<>();
+    private final int[] beeteArray = new int[]{
+            R.drawable.ic_android_black_24dp, R.drawable.ic_brightness_5_black_24dp, R.drawable.ic_local_car_wash_black_24dp, R.drawable.ic_sentiment_very_dissatisfied_black_24dp
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         beetListe.addLast("Bananen");
         beetListe.addLast("Tomaten");
@@ -32,13 +37,17 @@ public class MainActivity extends AppCompatActivity {
 
         myRecyclerView = findViewById(R.id.recyclerview); //Handler wird beschrieben
 
-        myBeeteListAdapter = new BeeteListAdapter(this, beetListe); // (Context context, LinkedList<String>)
+        myBeeteListAdapter = new BeeteListAdapter(this, beetListe, beeteArray); // (Context context, LinkedList<String>)
         //Konstruktor plus unsere Daten
         //ab hier läuft der adapter
 
         myRecyclerView.setAdapter(myBeeteListAdapter); //set adapter for the recycler view layout element
 
-        myRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)); //layout manager
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)); //layout manager
+
+        //Snap-scrolling
+        SnapHelper snapper = new LinearSnapHelper();
+        snapper.attachToRecyclerView(myRecyclerView);
 
 
     }
